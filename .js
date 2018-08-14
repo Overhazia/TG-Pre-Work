aee859b9e70529442fb0473f77d66b74ef20fb65
COOLEST\allen <allen.buckley@rocketmail.com>
2018-08-14 14:38:48 -0600

    js functions

--- .js
+++ .js
@@ -1,16 +1,4 @@
-let number = Math.floor(Math.random() * 1000);
-let early = true;
-const age = 18;
-if(early && age > 18){
-  number += 1000;
-}
+const volumeOfSphere = diameter => (1/6) * Math.PI * diameter * diameter * diameter;
 
-if(early && age > 18){
-  console.log( `Your race will begin at 9:30 and your race number is: ${number}.`);
-} else if (!early && age > 18){
-  console.log(`Your race will start at 11:00 and your race number is: ${number}.`);
-} else if (age < 18){
-  console.log(`Your race will start at 12:30 and your race number is: ${number}.`);
-} else {
-  console.log('Please see registration desk.');
-}
+
+console.log('The volume of a sphere is ' + volumeOfSphere(10) + ' cubic centimeters');
