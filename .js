5388b1bdf607376c4f9f1d531897d7822c71a96a
COOLEST\allen <allen.buckley@rocketmail.com>
2018-08-14 12:33:10 -0600

    kelvin weather

--- .js
+++ .js
@@ -1,32 +1,12 @@
-// This line of code sets the variable location to the string New York City
-const location = 'New York City';
-
-// This line of code sets the variable latitude to the number 40.7
-let latitude = 40.7;
-
-// This line of code sets the variable inNorthernHemisphere to true
-let inNorthernHemisphere = true;
-console.log(location);
-console.log(latitude);
-console.log(inNorthernHemisphere);
-const entree = 'Enchiladas';
-const price = 12
-console.log(entree , price);
-
-let changeMe = false;
-console.log(changeMe);
-let notDefined;
-console.log(notDefined);
-let valueless;
-console.log(valueless);
-let molecule = 16;
-molecule += 16;
-let particle = 8;
-particle *= 6.02;
-let assay = 15
-assay++;
-let favoriteAnimal = 'wild ass';
-console.log('My favorite animal ' + favoriteAnimal + '.');
-let myName = 'Allen';
-let myCity= 'longmont';
-console.log(`My name is ${myName}. My favorite city is ${myCity}.`)
+//value saved to kelvin will stay constant
+const kelvin = 293;
+//converts kelvin to celcius
+const celsius = kelvin - 273;
+//variable fahrenheit
+let fahrenheit = celsius * (9/5) + 32;
+//rounds down fahrenheit temp to nearest whole
+fahrenheit = Math.floor(fahrenheit);
+console.log(`The temperature is ${fahrenheit} degrees Fahrenheit.`);
+let newton = celsius * (33/100);
+newton = Math.floor(newton);
+console.log(`The temperature is ${newton} degrees newton`);
