fb8809fa1fdd2be616f1049155a93403c1bc61ab
COOLEST\allen <allen.buckley@rocketmail.com>
2018-08-14 12:31:05 -0600

    js variables

--- .js
+++ .js
@@ -1,10 +1,32 @@
+// This line of code sets the variable location to the string New York City
+const location = 'New York City';
 
-console.log('It was love at first sight.');
-/*
-console.log('The first time Yossarian saw the chaplain he fell madly in love with him.');
-console.log('Yossarian was in the hospital with a pain in his liver that fell just short of being jaundice.');
-console.log('The doctors were puzzled by the fact that it wasn\'t quite jaundice.');
-console.log('If it became jaundice they could treat it.');
-console.log('If it didn\'t become jaundice and went away they could discharge him.');
-console.log('But this just being short of jaundice all the time confused them.');
-*/
+// This line of code sets the variable latitude to the number 40.7
+let latitude = 40.7;
+
+// This line of code sets the variable inNorthernHemisphere to true
+let inNorthernHemisphere = true;
+console.log(location);
+console.log(latitude);
+console.log(inNorthernHemisphere);
+const entree = 'Enchiladas';
+const price = 12
+console.log(entree , price);
+
+let changeMe = false;
+console.log(changeMe);
+let notDefined;
+console.log(notDefined);
+let valueless;
+console.log(valueless);
+let molecule = 16;
+molecule += 16;
+let particle = 8;
+particle *= 6.02;
+let assay = 15
+assay++;
+let favoriteAnimal = 'wild ass';
+console.log('My favorite animal ' + favoriteAnimal + '.');
+let myName = 'Allen';
+let myCity= 'longmont';
+console.log(`My name is ${myName}. My favorite city is ${myCity}.`)
