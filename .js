e3f493773cf6b29a31168ffc49fbcad14e452dd1
COOLEST\allen <allen.buckley@rocketmail.com>
2018-08-14 12:37:18 -0600

    js control flow

--- .js
+++ .js
@@ -1,14 +1,13 @@
-// my current age
-const myAge = 22;
-// first 
-let earlyYears = 2;
-earlyYears *= 10.5;
-let laterYears = myAge - 2;
-// my later years in dog years
-laterYears *= 4;
-console.log(laterYears);
-console.log(earlyYears);
-let myAgeInDogYears = earlyYears + laterYears;
-console.log(myAgeInDogYears);
-let myName = 'Allen Buckley'.toLowerCase();
-console.log(`My name is ${myName}. I am ${myAgeInDogYears} years old in dog years.`);
+let isLocked = true;
+isLocked ? console.log('You will need a key to open the door.') :
+  console.log('You will not need a key to open the door.');
+
+
+let isCorrect = true;
+
+isCorrect ? console.log('Correct!') : console.log('Incorrect!');
+
+
+let favoritePhrase = 'Love That!';
+
+favoritePhrase ? console.log('I love that!') : console.log("I don't love that!");
