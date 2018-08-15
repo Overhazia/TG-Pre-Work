569bb2ebd8ca81e7a8d7982b3e409ca24dbf0b40
COOLEST\allen <allen.buckley@rocketmail.com>
2018-08-15 10:14:39 -0600

    js property access

--- .js
+++ .js
@@ -3,3 +3,7 @@ let bucketList = ['Rappel into a cave', 'Take a falconry class', 'Learn to juggl
 console.log(bucketList);
 let newYearsResolutions = ['Fix credit', 'Go camping more', 'Nail this apprenticeship'];
 console.log(newYearsResolutions);
+
+let listItem = ['Fix Credit'];
+console.log(listItem[0]);
+console.log(newYearsResolutions[2]);
