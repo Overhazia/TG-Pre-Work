292937da3621831ea730854019e4565d71d63e53
COOLEST\allen <allen.buckley@rocketmail.com>
2018-08-15 09:44:37 -0600

    js block scope II

--- .js
+++ .js
@@ -1,6 +1,12 @@
 const visibleLightWaves = () => {
     let lightWaves = 'Moonlight';
+    let region = 'The Arctic';
+    if (region === 'The Arctic'){
+      let lightWaves = 'northern Lights';
+      console.log(lightWaves);
+    }
+    
     console.log(lightWaves);
-  }
+  };
   visibleLightWaves('Moonlight');
-  console.log(lightWaves);
+  
