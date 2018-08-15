fe5e2ca7ddbe59e9e29ad95194c645bd3026fb3d
COOLEST\allen <allen.buckley@rocketmail.com>
2018-08-14 18:07:50 -0600

    js global scope

--- .js
+++ .js
@@ -1,4 +1,7 @@
-const volumeOfSphere = diameter => (1/6) * Math.PI * diameter * diameter * diameter;
-
-
-console.log('The volume of a sphere is ' + volumeOfSphere(10) + ' cubic centimeters');
+const satellite = 'The Moon';
+const galaxy = 'The Milky Way';
+let stars = 'North Star';
+const myNightSky = () => {
+  return 'Night Sky: ' + satellite + ',' + stars + ',' + galaxy;
+}
+console.log(myNightSky());
