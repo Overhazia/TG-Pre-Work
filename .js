b26a2557d2afa0a644c6d262a6536171f179312e
COOLEST\allen <allen.buckley@rocketmail.com>
2018-08-15 10:39:21 -0600

    js more array methods

--- .js
+++ .js
@@ -1,12 +1,6 @@
-let newYearsResolutions = ['Fix credit', 'Learn a new language', 'Nail this apprenticeship'];
-console.log(newYearsResolutions);
-
-let listItem = ['Fix Credit'];
-console.log(listItem[0]);
-console.log(newYearsResolutions[2]);
-console.log(newYearsResolutions.length);
-newYearsResolutions.push('New car', 'Make a baby');
-console.log(newYearsResolutions);
-
-newYearsResolutions.pop();
-console.log(newYearsResolutions);
+let groceryList = ['orange juice', 'bananas', 'coffee beans', 'brown rice', 'pasta', 'coconut oil', 'plantains'];
+groceryList.shift();
+console.log(groceryList);
+groceryList.unshift('popcorn');
+console.log(groceryList);
+console.log(groceryList.slice(1, 4));
