459d6cbafb4e70eb01953e45bccea7e559fbab05
COOLEST\allen <allen.buckley@rocketmail.com>
2018-08-15 10:29:21 -0600

    js push method

--- .js
+++ .js
@@ -1,9 +1,12 @@
-let bucketList = ['Rappel into a cave', 'Take a falconry class', 'Learn to juggle'];
-
-console.log(bucketList);
-let newYearsResolutions = ['Fix credit', 'Go camping more', 'Nail this apprenticeship'];
+let newYearsResolutions = ['Fix credit', 'Learn a new language', 'Nail this apprenticeship'];
 console.log(newYearsResolutions);
 
 let listItem = ['Fix Credit'];
 console.log(listItem[0]);
 console.log(newYearsResolutions[2]);
+console.log(newYearsResolutions.length);
+newYearsResolutions.push('New car', 'Make a baby');
+console.log(newYearsResolutions);
+
+newYearsResolutions.pop();
+console.log(newYearsResolutions);
