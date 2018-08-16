0a1834b132df3d02a7c5a2a7f27a8b67119cffe7
COOLEST\allen <allen.buckley@rocketmail.com>
2018-08-16 14:46:17 -0600

    js getter and setter

--- .js
+++ .js
@@ -1,5 +1,23 @@
 let person = {
   _name: 'Lu Xun',
-  _age: 137
+  _age: 137,
+  
+  set age(ageIn) {
+    if (typeof ageIn === 'number') {
+      this._age = ageIn;
+    }
+    else {
+      console.log('Invalid input');
+      return 'Invalid input';
+    }
+  },
 
+  get age() {
+    console.log(`${name} is ${age} years old.`);
+    return this._age;
+  }
 };
+
+
+person.age = 'Thirty-nine';
+person.age = 39;
