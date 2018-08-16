3ed043194d1dcbc7d10cfa5c278d12d61c380285
COOLEST\allen <allen.buckley@rocketmail.com>
2018-08-15 18:05:05 -0600

    js accessing object properties III

--- .js
+++ .js
@@ -1,15 +1,16 @@
-let cities = ['Nashville', 'Charlotte', 'Asheville', 'Austin', 'Boulder'];
-
-let nums = [1, 50, 75, 200, 350, 525, 1000];
-
-//  Choose a method that will return undefined
-cities.forEach(city => console.log('Have you visited ' + city + '?'));
-
-// Choose a method that will return a new array
-let longCities = cities.filter(city => city.length > 7);
-
-// Choose a method that will return a new array
-let smallerNums = nums.map(num => num - 5);
-
-// Choose a method that will return a boolean value
-nums.every(num => num < 0);
+let person = {
+    name: 'Allen',
+    age: 42,
+    weekendAlarm: 'No alarms needed',
+    weekAlarm: 'Alarm set to 7AM'
+  };
+  let day = 'Wednesday';
+    let alarm;
+  if (day === 'Saturday' || day === 'Sunday' ) {
+    alarm = 'weekendAlarm';
+  } else {
+    alarm = 'weekAlarm';
+  }
+  console.log(person[alarm]);
+  console.log(person['name']);
+  console.log(person['age']);
