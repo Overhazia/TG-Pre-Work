874b4e4571b1c33e9d6714de675a38f859c1477d
COOLEST\allen <allen.buckley@rocketmail.com>
2018-08-16 11:40:47 -0600

    keycode II

--- .js
+++ .js
@@ -1,16 +1,5 @@
 let person = {
-    name: 'Allen',
-    age: 42,
-    weekendAlarm: 'No alarms needed',
-    weekAlarm: 'Alarm set to 7AM'
-  };
-  let day = 'Wednesday';
-    let alarm;
-  if (day === 'Saturday' || day === 'Sunday' ) {
-    alarm = 'weekendAlarm';
-  } else {
-    alarm = 'weekAlarm';
-  }
-  console.log(person[alarm]);
-  console.log(person['name']);
-  console.log(person['age']);
+  _name: 'Lu Xun',
+  _age: 137
+
+};
