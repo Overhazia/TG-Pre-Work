4a79eabd1cf98e4a451bfda8f51a89bd150efb48
COOLEST\allen <allen.buckley@rocketmail.com>
2018-08-17 10:21:28 -0600

    JS methods

--- .js
+++ .js
@@ -1,32 +1,26 @@
-class Dog {
-  constructor(name) {
+class Surgeon {
+  constructor(name, department) {
     this._name = name;
-    this._behavior = 0;
+    this._department = department;
+    this._remainingVacationDays = 20;
   }
-
+  
   get name() {
     return this._name;
   }
-  get behavior() {
-    return this._behavior;
-  }   
-
-  incrementBehavior() {
-    this._behavior ++;
+  
+  get department() {
+    return this._department;
+  }
+  
+  get remainingVacationDays() {
+    return this._remainingVacationDays;
+  }
+  
+  takeVacationDays(daysOff) {
+    this._remainingVacationDays -= daysOff;
   }
 }
-const halley = new Dog('Halley');
-console.log(halley.name); // Print name value to console
-console.log(halley.behavior); // Print behavior value to console
-halley.incrementBehavior(); // Add one to behavior
-console.log(halley.name); // Print name value to console
-console.log(halley.behavior); // Print behavior value to console
 
-class Surgeon {
-  constructor(name, department) {
-    this.name = name;
-    this.department = department;
-  }
-}
 const surgeonCurry = new Surgeon('Curry', 'Cardiovascular');
 const surgeonDurant = new Surgeon('Durant', 'Orthopedics');
